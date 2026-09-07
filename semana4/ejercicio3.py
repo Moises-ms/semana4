7dff9eafd8b1ba06f1843f2f3ab11cf13587c7c1
Moises Moraga <momoraga@uamv.edu.ni>
2026-09-07 09:20:38 -0700

    Función para calcular el subtotal

--- semana4/ejercicio3.py
+++ semana4/ejercicio3.py
@@ -2,3 +2,8 @@
 product_name = input("Nombre del producto: ")
 product_price = float(input("Precio unitario: C$ "))
 product_quantity = int(input("Cantidad: "))
+
+def calculate_subtotal(price, quantity):
+    subtotal_val = price * quantity
+    return subtotal_val
+
